7d6cdef40667b520a4d160fb68d1c5144be60829
Tomasz Cieńciała <tomasz.cienciala@gmail.com>
2015-01-18 11:56:04 +0100

    Users can edit and destroy only their tweets now

--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class TweetsControllerTest < ActionController::TestCase
+include Devise::TestHelpers
+  
   setup do
     @tweet = tweets(:one)
   end
--- test/helpers/tweets_helper_test.rb
+++ test/helpers/tweets_helper_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
 
 class TweetsHelperTest < ActionView::TestCase
+	include Devise::TestHelpers
 end
